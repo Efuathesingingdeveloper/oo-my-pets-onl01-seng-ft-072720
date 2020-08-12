8cd09a5a74ad606ea9eee60d1a9c1b5c171ce674
Georgette Class-Peters <georgycp@udel.edu>
2020-08-12 01:52:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -72,7 +72,7 @@ class Owner
     num_dogs = @pets[:dogs].size
     num_cats = @pets[:cats].size
    
-    return "I have #{num_fish} fish, #{num_dogs} dog(s), and #{num_cats} cat(s)."
+    return "I have  #{num_dogs} dog(s), and #{num_cats} cat(s)."
   end
 
   # Class Methods #
