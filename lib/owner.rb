4ccbe6d1cabf7a1bc2d6c3c72f9c68b84292e042
Georgette Class-Peters <georgycp@udel.edu>
2020-08-12 01:53:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -8,7 +8,7 @@ class Owner
   def initialize(species)
     @species = species
     @@all << self
-    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @pets = { :dogs => [], :cats => []}
   end
 
   def say_species
@@ -16,10 +16,6 @@ class Owner
   end
 
   # Pets #
-
-  def buy_fish(name_of_fish)
-    @pets[:fishes] << Fish.new(name_of_fish)
-  end
 
   def buy_dog(name_of_dog)
     @pets[:dogs] << Dog.new(name_of_dog)
@@ -49,16 +45,7 @@ class Owner
     end
   end
 
-  def feed_fish
-    @pets.collect do |species, instances|
-      if species == :fishes
-        instances.each do |fish|
-          fish.mood = "happy"
-        end
-      end
-    end
-  end
-
+  
   def sell_pets
     @pets.collect do |species, instances|
       instances.each do |pet|
